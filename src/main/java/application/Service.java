82c341024f0d5c768ab7c078a9d456db4cbc5e6e
Azhar <tamboliazhar602@gmail.com>
2023-12-29 12:00:09 +0530

    1st commit

---
+++ src/main/java/application/Service.java
@@ -0,0 +1,13 @@
+package application;
+
+import java.util.List;
+
+public interface Service {
+
+    List<Product>displayAllProduct();
+
+    boolean placeOrder(Order newOrder);
+
+    boolean cancelOrder(int orderId);
+    List<OrderInfo>displayAllOrders();
+}
